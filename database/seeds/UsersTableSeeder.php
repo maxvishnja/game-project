ebd3d7cd108e1718c0ad1c616aedbfb453ba605f
Max <maxmobiles.ru@Maxmobilesrus-MacBook-Pro-4.local>
2019-08-30 13:31:24 +0300

    init project

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'Super Administrator';
+        $user->email = 'admin@admin.com';
+        $user->password = bcrypt('admin2019');
+        $user->remember_token = str_random(10);
+        $user->save();
+    }
+}
